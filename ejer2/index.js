c1edea3ec496381a745386ee0af5acc7eedb1982
JosuneRG <josunerg94@gmail.com>
2025-05-17 13:46:07 +0200

    Cambio realizado para modificar el readme.

--- ejer2/index.js
+++ ejer2/index.js
@@ -1,3 +1,12 @@
+//Pasos seguidos:
+//Terminal VS code -- Mkdir para crear la carpeta
+//cd ejer2
+// incializar el proyecto: npm init -y
+//En package.json cambiar el name 
+//Terminal poner esto: npm install logplease --save-dev
+// Y en el package.json. poner en scripts --> "start": "node index.js"
+// ejecutar el proyecto --> npm start
+
 const Logger = require('logplease');
 const logger = Logger.create('Ejercicio2');
 
